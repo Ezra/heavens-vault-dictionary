d068f9f307820eed9e6321e5f939d43c60bc077a
Ezra <ezra.bradford@gmail.com>
2020-06-01 13:32:34 -0700

    apply autopep8 formatting

--- xor-exploration.py
+++ xor-exploration.py
@@ -2,16 +2,16 @@ import json
 import sys
 
 
-def xorfile(input, output="", prefix = "decrypted_"):
+def xorfile(input, output="", prefix="decrypted_"):
     if input.startswith(prefix):
         output = input[len(prefix):]
     else:
-        output= prefix+input
-    with open(output,'wb') as f:
-        with open(input,'rb') as g:
+        output = prefix+input
+    with open(output, 'wb') as f:
+        with open(input, 'rb') as g:
             byte = g.read(1)
             while len(byte) == 1:
-                f.write(bytes([ord(byte)^97]))
+                f.write(bytes([ord(byte) ^ 97]))
                 byte = g.read(1)
 
 
@@ -25,7 +25,8 @@ def main():
     word_dict = j["player"]["lexDictionary"]["_lexDictionary"]
     words = list(word_dict.keys())
     known_words = [i for i in word_dict if word_dict[i].get("known", False)]
-    wrong_words = [(i,word_dict[i]["_suggestedWordString"]) for i in word_dict if word_dict[i].get("_suggestedWordString", "") != i]
+    wrong_words = [(i, word_dict[i]["_suggestedWordString"])
+                   for i in word_dict if word_dict[i].get("_suggestedWordString", "") != i]
 
     print(f'len(words)')
     print(f'len(known_words)')
