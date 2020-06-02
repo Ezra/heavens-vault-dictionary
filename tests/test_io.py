1472c6e1c39a8ca901e27a064afe249e28c9da9d
Ezra Bradford <ezra.bradford@gmail.com>
2020-06-02 15:57:00 -0700

    extend test to more variations

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,16 +6,28 @@ from pathlib import Path
 class Test_Encryption(unittest.TestCase):
     def setUp(self):
         self.data_path = Path.cwd() / 'tests' / 'data'
-        self.encrypted_input_path = self.data_path / 'encrypted.json'
-        self.decrypted_input_path = self.data_path / 'unencrypted.json'
+        self.encrypted_input_path = self.data_path / 'save1.json'
+        self.decrypted_input_path = self.data_path / 'decrypted_save2.json'
         self.encrypted_output_path = (
-            self.data_path / 'decrypted_encrypted.json')
+            self.data_path / 'save2.json')
         self.decrypted_output_path = (
-            self.data_path / 'decrypted_unencrypted.json')
+            self.data_path / 'decrypted_save1.json')
 
-    def test_encrypt_filename(self):
+    def test_encrypt_filename_path(self):
         encrypted = xorfile(str(self.decrypted_input_path))
         self.assertEqual(encrypted, str(self.encrypted_output_path))
+
+    def test_encrypt_filename_string(self):
+        encrypted = xorfile(str(self.decrypted_input_path))
+        self.assertEqual(encrypted, str(self.encrypted_output_path))
+
+    def test_decrypt_filename_path(self):
+        decrypted = xorfile(self.encrypted_input_path)
+        self.assertEqual(decrypted, self.decrypted_output_path)
+
+    def test_decrypt_filename_string(self):
+        decrypted = xorfile(self.encrypted_input_path)
+        self.assertEqual(decrypted, self.decrypted_output_path)
 
 
 if __name__ == '__main__':
