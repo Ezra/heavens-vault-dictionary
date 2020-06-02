c37c72e6bcbff67601019f2817154c0c2c279097
Ezra Bradford <ezra.bradford@gmail.com>
2020-06-02 16:48:55 -0700

    don't support string mode

--- tests/test_io.py
+++ tests/test_io.py
@@ -26,21 +26,13 @@ class Test_Encryption(unittest.TestCase):
                 ]:
             output_path.unlink(missing_ok=True)
 
-    def test_encrypt_filename_path(self):
+    def test_encrypt_filename(self):
         encrypted = xorfile(self.decrypted_input_path)
         self.assertEqual(encrypted, self.encrypted_output_path)
 
-    def test_encrypt_filename_string(self):
-        encrypted = xorfile(str(self.decrypted_input_path))
-        self.assertEqual(encrypted, str(self.encrypted_output_path))
-
-    def test_decrypt_filename_path(self):
+    def test_decrypt_filename(self):
         decrypted = xorfile(self.encrypted_input_path)
         self.assertEqual(decrypted, self.decrypted_output_path)
-
-    def test_decrypt_filename_string(self):
-        decrypted = xorfile(str(self.encrypted_input_path))
-        self.assertEqual(str(decrypted, self.decrypted_output_path))
 
 
 if __name__ == '__main__':
