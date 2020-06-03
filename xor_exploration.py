9df275d2afd0621d437eb87441843663d14d2514
Ezra Bradford <ezra.bradford@gmail.com>
2020-06-02 20:12:07 -0700

    document prefix toggling

--- xor_exploration.py
+++ xor_exploration.py
@@ -29,7 +29,10 @@ HEADERS = {
     }
 
 
-def toggle_prefix(text, prefix, /):
+def toggle_prefix(text, prefix):
+    ''' If the string starts with the prefix,
+    remove it. Otherwise, insert it.
+    '''
     if text.startswith(prefix):
         # 3.8: slice off prefix
         # 3.9: removeprefix()
